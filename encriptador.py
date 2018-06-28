6ee2f4381d9e817365940977235be3de5fbf260a
cristianurra <40528744+cristianurra@users.noreply.github.com>
2018-06-28 18:34:51 -0400

    Update encriptador.py

--- encriptador.py
+++ encriptador.py
@@ -1,39 +1,57 @@
 import socket
+import sys
 dic="abcdefghijklmnopqrstuvwxyz1234567890,.;:{}[]+*!#$%&/()=?abcdefghijklmnopqrstuvwxyz1234567890,.;:{}[]+*!#$%&/()=?abcdefghijklmnopqrstuvwxyz1234567890,.;:{}[]+*!#$%&/()=?"
 
-def enviar(ip,puerto,msg):
+def enviar(ip,puertodestino,msg):
     sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
-    sock.sendto(msg, (ip, puerto))
+    sock.sendto(msg, (ip, puertodestino))
 
 
 
-def recibir(ip,puerto):
+def recibir(ip,puertoorigen):
     sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-    sock.bind((ip, puerto))
+    sock.bind((ip, puertoorigen))
 
 
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     f=str(data)
-    print encriptar(f,-clave)
+    print desencriptar(f,clave)
 
 
 
 def encriptar(texto,clave):
     l=list(texto)
     n=0
+    j=1
     while n<len(l):
           k=dic.find(l[n])
           if l[n]==" ":
              l[n]=" "
           if l[n]!=" ":
-             l[n]=dic[k+int(clave)]
+             l[n]=dic[k+int(clave+j)]
+          j+=1
           n+=1
     f="".join(l)
     return f
 
-ip=raw_input("ip: ")
+def desencriptar(texto,clave):
+    l=list(texto)
+    n=0
+    j=1
+    while n<len(l):
+          k=dic.find(l[n])
+          if l[n]==" ":
+             l[n]=" "
+          if l[n]!=" ":
+             l[n]=dic[k-int(clave+j)]
+          j+=1
+          n+=1
+    f="".join(l)
+    return f
+
+ipclear=raw_input("ip: ")
 ip="127.0.0.1"
 puertoorigen=int(raw_input("puerto de origen: "))
 puertodestino=int(raw_input("puerto de destino: "))
@@ -48,7 +66,6 @@ enviar(ip,puertodestino,msg)
 
 while True:
       recibir(ip,puertoorigen)
-
-      msg=raw_input("Texto: ")
+      msg=raw_input("> ")
       msg=encriptar(msg,clave)
       enviar(ip,puertodestino,msg)
